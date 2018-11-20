0d26d95341f0daca35df393b0ccebecb9f779d63
rivara <ricardo.v.ramirez@gmail.com>
2018-11-20 20:56:44 +0100

    Intento de warpping en  la actividad maps

--- app/src/main/java/com/example/ricardovalleramire/myapplication/Map.java
+++ app/src/main/java/com/example/ricardovalleramire/myapplication/Map.java
@@ -5,9 +5,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.widget.RelativeLayout;
+
 public class Map extends AppCompatActivity {
 
 
@@ -20,24 +23,28 @@ public class Map extends AppCompatActivity {
 
         setContentView(R.layout.map);
         Button firstButton = (Button) findViewById(R.id.btnBack);
+
+      //preguntar en stackoverflow como encapsular un mapa
         // Default google map
         //get values from bbdd
-        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(
-                "http://maps.google.com/maps?q=loc:51.5, 0.125"));
+        // Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(
+        // "http://maps.google.com/maps?q=loc:51.5, 0.125"));
 
-       startActivity(intent);
-        //myHabitsMap=intent();
-        //intentar hacer lo mismo en lugar de btn id del map
-        //Button firstButton = (Button) findViewById(R.id.btnBack);
+        // web on_blank
+        //WebView mWebView = (WebView) findViewById(R.id.view);
+        //mWebView.getSettings().setJavaScriptEnabled(true);
+        //mWebView.getSettings().setMinimumFontSize(1);
+        //mWebView.getSettings().setMinimumLogicalFontSize(1);
+        //mWebView.loadUrl("http://www.google.com");
+
+
 
         firstButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Log.d(TAG, "onClick: Clicked Button One!");
-                //toastMessage("Clicked Button One");
                 Intent intent = new Intent(Map.this, MainActivity.class);
                 startActivity(intent);
-                //intent= findViewById(R.id.mapView);
+
             }
         });
 
